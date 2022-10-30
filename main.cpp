e59454a6e9e406f0d7ae50751ed622a0a8c728da
Shiroyasha <vishesh2603@gmail.com>
2022-10-30 09:50:01 +0530

    Added the sphere, finally we have an object (though yes the it has it errors like if you send the ball behind the cam it will mess up)

--- main.cpp
+++ main.cpp
@@ -5,8 +5,20 @@
 #include <iostream>
 #include <math.h>
 
+bool hit_sphere(const point3& center, const double radius, const ray& r)
+{
+	vec3 oc = r.orig - center;
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0 * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4.0 * a * c;
+	return (discriminant > 0);
+}
+
 color ray_color(const ray& r)
 {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+		return color(1, 0, 0);
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5 * (unit_direction.y() + 1.0);
 
