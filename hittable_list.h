20b08882fd386dae503b2d5cbf52581b109c41ca
Shiroyasha <vishesh2603@gmail.com>
2022-12-09 16:32:50 +0530

    Added BVH

--- hittable_list.h
+++ hittable_list.h
@@ -2,6 +2,7 @@
 #define HITTABLE_LIST_H
 
 #include "hittable.h"
+#include "aabb.h"
 
 #include <memory> // For shared_ptr
 #include <vector>
@@ -19,6 +20,7 @@ public:
 	void add(shared_ptr<hittable> object) { objects.push_back(object); }
 
 	virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+	virtual bool bounding_box(double time0, double time1, aabb& output_box) const override;
 
 public:
 	std::vector<shared_ptr<hittable>> objects;
@@ -38,6 +40,21 @@ bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& re
 	}
 
 	return hit_anything;
+}
+
+bool hittable_list::bounding_box(double time0, double time1, aabb& output_box) const{
+	if (objects.empty()) return false;
+
+	aabb temp_box;
+	bool first_box = true;
+
+	for (const auto& object : objects) {
+		if (!object->bounding_box(time0, time1, temp_box)) return false;
+		output_box = first_box ? temp_box : surrounding_box(temp_box, output_box);
+		first_box = false;
+	}
+
+	return true;
 }
 
 #endif
